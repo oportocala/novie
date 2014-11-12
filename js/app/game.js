88876730684b304c7ec8c5e37e9a80ccc1b18d39
Vlad Goran <oportocala@gmail.com>
2014-11-12 22:26:32 +0200

    back to basics

--- js/app/game.js
+++ js/app/game.js
@@ -1,22 +1,28 @@
-var manager = new THREE.LoadingManager();
-manager.onProgress = function (item, loaded, total) {
-	console.log(item, loaded, total);
-};
-
-var renderer = new THREE.WebGLRenderer();
 var scene = new THREE.Scene();
-//var camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
-var aspect = window.innerWidth / window.innerHeight;
-var d = 20;
-camera = new THREE.OrthographicCamera(-d * aspect, d * aspect, d, -d, 1, 1000);
-camera.position.x = d;
-camera.position.y = d;
-camera.position.z = 0;
-
-
+var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementsByClassName('scene-wrapper').item(0).appendChild(renderer.domElement);
+
+var usePerspective = true;
+
+if (usePerspective) {
+	var camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 0.1, 1000 );
+	camera.position.x = 60;
+	camera.position.y = 80;
+	camera.position.z = 0;
+
+	camera.lookAt(scene.position);
+} else {
+	var aspect = window.innerWidth / window.innerHeight;
+	var d = 20;
+	camera = new THREE.OrthographicCamera(-d * aspect, d * aspect, d, -d, 1, 1000);
+	camera.position.x = d;
+	camera.position.y = d;
+	camera.position.z = 0;
+
+	camera.lookAt(scene.position);
+}
+
 
 var updateFcts = [];
 var worldX = new THREEx.CannonWorld().start();
@@ -68,25 +74,9 @@ var generateSphere = function (color, y, name) {
 
 var plane = function generatePlane() {
 
-	var loader = new THREE.OBJLoader(manager);
-	loader.load('models/plane.obj', function (object) {
-		object.traverse(function (child) {
-			if (child instanceof THREE.Mesh) {
-				//child.material.map = texture;
-			}
-		});
-
-
-
-	}, function () {
-		console.log('progress', arguments)
-	}, function () {
-		console.log('error', arguments)
-	});
-
 
 	var planeSize = 50;
-	var geometry = new THREE.Geometry();
+	var geometry = new THREE.CubeGeometry(planeSize, 1, planeSize, 1, 1, 1);
 	var material = new THREE.MeshBasicMaterial({color: 0xffffff});
 
 	var mesh = new THREE.Mesh(geometry, material);
@@ -111,8 +101,7 @@ var plane = function generatePlane() {
 	physicsBody.body.addEventListener("collide", function (e) {
 		if (e.with.userData.object3d.isSphere) {
 			console.log(e.with.userData.object3d.name, 'collided with', name);
-			sound.play('impact');
-			physicsBody.body.sleep();
+			//sound.play('impact');
 		}
 	});
 
@@ -164,7 +153,6 @@ var generateControls = function (target, keys) {
 	return function () {
 		var speed = 20;
 		if (keyboard.pressed(keys.left)) {
-
 			target.body.angularVelocity.x = speed;
 		} else if (keyboard.pressed(keys.right)) {
 			target.body.angularVelocity.x = -speed;
@@ -197,13 +185,18 @@ var initScene = function () {
 initScene();
 
 
-var deathY = -30;
+
 
 var sphereDied = function (target) {
 	target.isDead = true;
 	console.log(target.name, 'died');
 	sound.play('death');
 	resetScene();
+};
+
+var resetScene = function () {
+	resetSphere(redSphere);
+	resetSphere(blueSphere);
 };
 
 var resetSphere = function (target) {
@@ -216,11 +209,8 @@ var resetSphere = function (target) {
 	target.body.velocity.z = 0;
 };
 
-var resetScene = function () {
-	resetSphere(redSphere);
-	resetSphere(blueSphere);
-};
 
+var deathY = -30;
 
 updateFcts.push(function () {
 
@@ -234,7 +224,7 @@ updateFcts.push(function () {
 
 });
 
-camera.lookAt(scene.position);
+
 updateFcts.push(function () {
 
 	if (cameraTarget) {
